49de42cf701238062c811121c6b3affb1a9e4f83
lastusername <justindm1914@gmail.com>
2024-09-02 23:08:38 -0400

    finalized fs module project Created a File System object Used the read directory function Prints out the files in the current directory

--- Learning/NodeJS/Modules/fs.js
+++ Learning/NodeJS/Modules/fs.js
@@ -1,5 +1,10 @@
 
 const fs = require("fs");
+// const files = fs.readdirSync('./');
+// console.log(files); 
 
-const files = fs.readdirSync('./');
-console.log(files); 
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log("result", files);
+
+});
