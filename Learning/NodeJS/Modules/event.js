2eb16fd6842af8693cdb33c86b5cf041bf1012dd
lastusername <justindm1914@gmail.com>
2024-09-04 11:26:56 -0400

    Updated Event listener and raiser

--- Learning/NodeJS/Modules/event.js
+++ Learning/NodeJS/Modules/event.js
@@ -3,12 +3,23 @@ const EventEmitter = require('events');
 const emitter = new EventEmitter();
 
 //Register a listener
-emitter.on("messageLogged", function(){
-    console.log('Listener called');
+emitter.on("messageLogged", (eventArg) => { //e, eventArg, arg are other naming options
+    console.log('Listener called', eventArg);
 });
 
-//raise an event
-emitter.emit("messageLogged");
+const log = require("../Lesson_1/logger.js");
+log("message");
 
+
+
+
+
+emitter.on("logging", (arg)=> {
+    console.log('logging...', arg);
+});
 // Emit means making a noise or producting - singaling that an event occured
-// we need a listener, it is a function that is called when the function is raised (emit)
+// we need a listener, it is a functeion that is called when the function is raised (emit)
+
+emitter.emit("logging", {data: 'Message'})
+
+// Rasie: logging (data: message)
