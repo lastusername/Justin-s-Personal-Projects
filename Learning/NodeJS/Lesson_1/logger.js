de977ea58f464ebe5979af6becd153e8bc243445
lastusername <justindm1914@gmail.com>
2024-10-04 23:42:14 -0400

    adding an event listener

--- Learning/NodeJS/Lesson_1/logger.js
+++ Learning/NodeJS/Lesson_1/logger.js
@@ -1,12 +1,17 @@
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
 var url ="http://mylogger.com/log" ;
 
 function log1(message) {
-
+    //send an HTTP request
     console.log(message);
+
+    //raise an event
+    emitter.emit("messageLogged", {id: 1, url: 'http://'});
 }
 function log2() {
     console.log("Message from node");
-
 }
 
 module.exports.log = {
